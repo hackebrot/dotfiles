6a0a0b58b6c7afd067a646e4a55c35139e121fd7
Raphael Pierzina <raphael@hackebrot.de>
2015-11-16 23:45:37 +0100

    Download git-completion from github

--- install.bash
+++ install.bash
@@ -3,3 +3,8 @@
 set -e # Exit if an error occurs
 
 set -u # Exit for an undefined variable
+
+# git-completion
+if [ ! -e ~/git-completion.bash ]; then
+  curl -o ~/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+fi
