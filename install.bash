8f9490c474fde08fc2a6b77f23c78db98b8b81ad
Raphael Pierzina <raphael@hackebrot.de>
2015-11-17 00:31:50 +0100

    Use double brackets for if-clause

--- install.bash
+++ install.bash
@@ -5,11 +5,11 @@ set -e # Exit if an error occurs
 set -u # Exit for an undefined variable
 
 # git-completion
-if [ ! -e ~/git-completion.bash ]; then
+if [[ ! -e ~/git-completion.bash ]]; then
   curl -o ~/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 fi
 
 # git-prompt
-if [ ! -e ~/git-prompt.sh ]; then
+if [[ ! -e ~/git-prompt.sh ]]; then
   curl -o ~/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 fi
