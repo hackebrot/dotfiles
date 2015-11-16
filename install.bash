8ed9271ff2350e0581d54441026ca18edc4dd0b8
Raphael Pierzina <raphael@hackebrot.de>
2015-11-16 23:46:52 +0100

    Download git-prompt from github

--- install.bash
+++ install.bash
@@ -8,3 +8,8 @@ set -u # Exit for an undefined variable
 if [ ! -e ~/git-completion.bash ]; then
   curl -o ~/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 fi
+
+# git-prompt
+if [ ! -e ~/git-prompt.sh ]; then
+  curl -o ~/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+fi
