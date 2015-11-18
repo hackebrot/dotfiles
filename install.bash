cb43c45921491023f9d727226f0cee2a35661789
Raphael Pierzina <raphael@hackebrot.de>
2015-11-18 23:16:40 +0100

    Get homebrew if it is not installed yet

--- install.bash
+++ install.bash
@@ -4,6 +4,11 @@ set -e # Exit if an error occurs
 
 set -u # Exit for an undefined variable
 
+# homebrew
+if [[ $(which -s brewz) != 0 ]]; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 # git-completion
 if [[ ! -e ~/git-completion.bash ]]; then
   curl -o ~/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
