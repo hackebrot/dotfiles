77593e5ec9802350936e49bc0acbdad22bdb7b34
Raphael Aurich <git@raphael.codes>
2025-07-15 14:14:32 +0200

    Update neovim colorscheme

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,22 @@
+return {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false, -- make sure we load this during startup
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+        require("catppuccin").setup({
+            color_overrides = {
+                frappe = {
+                    base = "#3a3a3a",
+                    mantle = "#3a3a3a",
+                    text = "#d2d2d2",
+                    lavender = "#c6c6c6",
+                },
+            },
+            custom_highlights = {
+                LineNr = { fg = "#7c7c7c" },
+            },
+        })
+        vim.cmd("colorscheme catppuccin-frappe")
+    end,
+}
