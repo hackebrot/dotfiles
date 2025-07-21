8e322d633fcce8b506a46709151fafcedcc35b11
Raphael Aurich <git@raphael.codes>
2025-07-21 11:19:03 +0200

    Add live grep args for nvim-telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,12 +1,49 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
+            'nvim-telescope/telescope-live-grep-args.nvim',
+            version = "^1.0.0",
+        },
+    },
     config = function()
         local builtin = require('telescope.builtin')
+        local telescope = require('telescope')
+
+        -- Access the live_grep_args functions
+        local lga_actions = require('telescope-live-grep-args.actions')
+        local lga_shortcuts = require('telescope-live-grep-args.shortcuts')
+
+        -- First setup telescope (as recommended in the docs)
+        telescope.setup({
+            extensions = {
+                live_grep_args = {
+                    auto_quoting = true, -- Enable auto-quoting
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = lga_actions.quote_prompt(),
+                            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                            ["<C-t>"] = lga_actions.quote_prompt({ postfix = " -t" }),
+                        },
+                    },
+                }
+            }
+        })
+
+        -- Then load the extension (after setup)
+        telescope.load_extension('live_grep_args')
+
+        -- Core telescope mappings
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+        -- Live grep args extension mappings
+        vim.keymap.set('n', '<leader>fg', telescope.extensions.live_grep_args.live_grep_args,
+            { desc = 'Telescope live grep (with args)' })
+        vim.keymap.set('n', '<leader>fs', lga_shortcuts.grep_word_under_cursor,
+            { desc = 'Telescope grep word under cursor' })
     end,
 }
