fa861cbce10a34875176fe6156574198bb278951
Raphael Aurich <git@raphael.codes>
2026-05-01 17:34:37 +0200

    fix(nvim): migrate treesitter to main branch to fix 0.12 crashes

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,27 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-        local configs = require("nvim-treesitter.configs")
+        local parsers = {
+            "go", "python", "lua", "json", "yaml",
+            "markdown", "markdown_inline",
+        }
+        local filetypes = {
+            "go", "python", "lua", "json", "yaml",
+            "markdown",
+        }
 
-        configs.setup({
-            ensure_installed = { "go", "python" },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
+        require("nvim-treesitter").install(parsers)
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = filetypes,
+            callback = function(args)
+                vim.treesitter.start(args.buf)
+                vim.bo[args.buf].indentexpr =
+                    "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
         })
-    end
+    end,
 }
